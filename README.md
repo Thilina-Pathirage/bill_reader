72dc09b81f063d48d2210bff9800fc478cf76d67
Dasun <dasunpathirage123@gmail.com>
2021-07-23 21:13:35 +0530

    updated main order

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # bill_reader
 
-Tutorial: https://nanonets.com/blog/ocr-with-tesseract/#detectonlydigits
+
+## Python 
+version 3.7* or (3.8)
+
+## Install Easyocr 
+```python3 -m pip install --upgrade pip```
+```pip install easyocr```
+
+## csv entries
+Received date	Supplier Name	Invoice Number	Invoice Date	Currency	Sub total	Vat/Tax	Total
+
+## Date and time validate format
+```"Jun 28 2018 at 7:40AM" -> "%b %d %Y at %I:%M%p"```
+```"September 18, 2017, 22:19:55" -> "%B %d, %Y, %H:%M:%S"```
+```"Sun,05/12/99,12:30PM" -> "%a,%d/%m/%y,%I:%M%p"```
+```"Mon, 21 March, 2015" -> "%a, %d %B, %Y"```
+```"2018-03-12T10:12:45Z" -> "%Y-%m-%dT%H:%M:%SZ"```
+https://stackabuse.com/converting-strings-to-datetime-in-python
+```dtime = datetime.datetime.now()```
--- main.py
+++ main.py
@@ -1,11 +1,47 @@
+##################################################################################################
+# Import Libraries
+
 import datetime
 import easyocr
 import csv
+from dateutil.parser import parse
+#import datetime as date_time_obj
+#from datetime import datetime
 
-IMAGE_PATH = './images/invoice.png'
+##################################################################################################
+# Functions
 
+# Date validate
+def is_date(date_time_str):
+    done = False
+    if done == False:
+        try: 
+            datetime.datetime.strptime(date_time_str, '%m/%d/%y')
+            done = True
+        except ValueError:
+            done = False
+    if done == False:
+        try: 
+            datetime.datetime.strptime(date_time_str, '%d/%m/%y')
+            done = True
+        except ValueError:
+            done = False
+    if done == False:
+        try: 
+            datetime.datetime.strptime(date_time_str, '%d/%m/%Y')
+            done = True
+        except ValueError:
+            done = False
+    return done
+    
+
+
+
+##################################################################################################
+
+# OCR 
+IMAGE_PATH = './images/invoice2.png'
 reader = easyocr.Reader(['en'], gpu=False)
-
 bound = reader.readtext(IMAGE_PATH)
 
 text = ''
@@ -13,42 +49,42 @@ for i in range(len(bound)):
     text = text + bound[i][1] + '\n'
 
 lower_text = text.lower()
-res = lower_text.split()
-
-# print(res)
+res = lower_text.split() # put in to list
+print(res)
 
 # Get total
 total = 0
-for index, i in enumerate(res):
-    if i == 'total':
-        print("The Total: " + res[index + 1])
-        total = res[index + 1]
-
-# Date validate
-
-
-def date_validate(date_string):
-    date_date = str(date_string)
-    try:
-        if date_string == datetime.datetime.strptime(date_date, '%Y-%m-%d'):
-            raise ValueError
-        return True
-    except ValueError:
-        return False
-
-
 date = "YYYY-MM-DD"
-for index, i in enumerate(res):
-    if date_validate(res[index]):
-        print("true")
-        date = res[index]
+Currency = "Currency"
+for index, value in enumerate(res):
+    # Get Total
+    if value == 'total':
+        tot_str = res[index + 1]
+        if tot_str[0] == 's':
+            Currency = "Doller"
+            total = tot_str[1:]
+        print("Total: " + total)
+    # Get Date
+    if is_date(value):
+        date = value
+        print("Date: " + date)
 
-header = ['Total', 'Date']
+
+
+
+""" for index, val in enumerate(res):
+    if val==val:
+        print("true") """
+        
+    
+
+header = ['Total', 'Date', 'Currency']
 data = [
-    [total, date]
+    [total, date, Currency]
 ]
 
 with open('outputs.csv', 'w', encoding='UTF8', newline='') as f:
     writer = csv.writer(f)
     writer.writerow(header)
     writer.writerows(data)
+
